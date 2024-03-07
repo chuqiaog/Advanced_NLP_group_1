f73fd7633ae507c0d79ccfeff7f9d90ffecf0e96
yjz02vu <156460532+yjz02vu@users.noreply.github.com>
2024-03-07 20:44:40 +0100

    Add files via upload

--- A3/baseline_ds.py
+++ A3/baseline_ds.py
@@ -142,3 +142,29 @@ def class_results(predictions, labels):
     results = metric.compute(predictions=reverse_label(true_predictions), references=reverse_label(true_labels))
     return results
 
+# --advanced
+def merge_into_sentences(sent_list):
+
+    word_sentlist = []
+    for sent in sent_list:
+        featdict = {}
+        token_list=[]
+        label_list=[]
+        pred_list=[]
+        for d in sent:
+            if d["V"] != '_':
+                token_list.append("Mark")
+                label_list.append(label_dict["_"])
+                token_list.append(d["form"])
+                label_list.append(label_dict[d["ARG"]])
+                token_list.append("Mark")
+                label_list.append(label_dict["_"])
+                pred_list.append(d["form"])
+            else:
+                token_list.append(d["form"])
+                label_list.append(label_dict[d["ARG"]])
+
+        featdict['tokens'],featdict['srl_arg_tags'],featdict['pred'] = token_list,label_list,pred_list
+        word_sentlist.append(featdict)
+    return word_sentlist
+    
